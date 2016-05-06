0cdaff2d9ea173fa40cad3c67f7feb61fa84dbcc
cpiccion <carmelo.piccione@theice.com>
2016-05-06 13:40:36 -0400

    change functor to allow for different outputs

--- src/gpr/gpr_model_plot.ml
+++ src/gpr/gpr_model_plot.ml
@@ -1,8 +1,15 @@
-module Make(F:Feature.S) =
+module Make(Inputs:Feature.S)(Outputs:Feature.S) =
 struct
-  module Model = Gpr_model.Make_with_stats(F)
+  module Model = Gpr_model.Make(Inputs)(Outputs)
   let plot ?args ~(data:Sampler.t) = 
     let args = match args with Some a -> a | None -> Gpr_model.Args.get() in
     Model.eval data args 
 end
+
+module Make_with_stats(Inputs:Feature.S) = 
+struct
+  include Make(Inputs)(Stat_feature)
+end
+
+
 
